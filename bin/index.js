83964b56c89833bece51e701c64b83935c0dec62
rohye <rohy.froze@gmail.com>
2021-01-28 15:48:19 +0800

    Use commander.js

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
+const { program } = require('commander')
+const figlet = require('figlet')
 const { log } = require('../src/utils')
 const interface = require('../src/interface')
 
-interface.init();
+log('blueBright', figlet.textSync('ROMANDAR'))
+
+program.version('1.0.0', '-v, --version')
+
+program.parse(process.argv)
